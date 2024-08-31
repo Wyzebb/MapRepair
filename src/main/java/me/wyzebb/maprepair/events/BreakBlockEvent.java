29bc8e0c4e3c5a95f4e5da0b252e12ae45fd78ee
Wyzebb <BenSJSmith8@gmail.com>
2024-08-31 23:59:05 +0100

    Add block break listener to test plugin

--- src/main/java/me/wyzebb/maprepair/events/BreakBlockEvent.java
+++ src/main/java/me/wyzebb/maprepair/events/BreakBlockEvent.java
@@ -1,4 +1,12 @@
 package me.wyzebb.maprepair.events;
 
-public class BlockBreakEvent {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class BreakBlockEvent implements Listener {
+    @EventHandler
+    private void onBlockBreak(BlockBreakEvent e) {
+        e.getPlayer().sendMessage("You broke " + e.getBlock());
+    }
 }
