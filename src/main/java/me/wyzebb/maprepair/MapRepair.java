5d6cd9c8b113191d5388b6040e0a4c79d7919268
Wyzebb <BenSJSmith8@gmail.com>
2024-09-01 00:02:54 +0100

    Add event and command registration

--- src/main/java/me/wyzebb/maprepair/MapRepair.java
+++ src/main/java/me/wyzebb/maprepair/MapRepair.java
@@ -1,13 +1,27 @@
-package me.wyzebb.mapRepair;
+package me.wyzebb.maprepair;
 
+import me.wyzebb.maprepair.commands.CommandManager;
+import me.wyzebb.maprepair.events.BreakBlockEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class MapRepair extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Plugin started!");
 
+        // Config
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+
+        // Register join and leave events
+        getServer().getPluginManager().registerEvents(new BreakBlockEvent(), this);
+
+        // Register commands and tab completer
+        Objects.requireNonNull(getCommand("maprepair")).setExecutor(new CommandManager(this));
+        Objects.requireNonNull(getCommand("maprepair")).setTabCompleter(new CommandManager(this));
     }
 
     @Override
