e42f03290ff3c2d1af885dfeecd1de7b1f68e3cb
Wyzebb <BenSJSmith8@gmail.com>
2024-09-01 11:41:56 +0100

    Update main class by getting an instance of BlockDataHandler

--- src/main/java/me/wyzebb/maprepair/MapRepair.java
+++ src/main/java/me/wyzebb/maprepair/MapRepair.java
@@ -2,11 +2,15 @@ package me.wyzebb.maprepair;
 
 import me.wyzebb.maprepair.commands.CommandManager;
 import me.wyzebb.maprepair.events.BreakBlockEvent;
+import me.wyzebb.maprepair.events.PlaceBlockEvent;
+import me.wyzebb.maprepair.utility.BlockDataHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
 public final class MapRepair extends JavaPlugin {
+
+    private BlockDataHandler blockDataHandler;
 
     @Override
     public void onEnable() {
@@ -16,16 +20,25 @@ public final class MapRepair extends JavaPlugin {
         saveDefaultConfig();
         getConfig().options().copyDefaults(true);
 
-        // Register join and leave events
-        getServer().getPluginManager().registerEvents(new BreakBlockEvent(), this);
+        // Initialize BlockDataHandler
+        blockDataHandler = new BlockDataHandler(this);
+        blockDataHandler.setupServerStartData();
 
-        // Register commands and tab completer
-        Objects.requireNonNull(getCommand("maprepair")).setExecutor(new CommandManager(this));
-        Objects.requireNonNull(getCommand("maprepair")).setTabCompleter(new CommandManager(this));
+        // Register join and leave events
+        getServer().getPluginManager().registerEvents(new BreakBlockEvent(this), this);
+        getServer().getPluginManager().registerEvents(new PlaceBlockEvent(this), this);
+        CommandManager commandManager = new CommandManager(this);
+        Objects.requireNonNull(getCommand("maprepair")).setExecutor(commandManager);
+        Objects.requireNonNull(getCommand("maprepair")).setTabCompleter(commandManager);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        blockDataHandler.serverShut();
+    }
+
+    public BlockDataHandler getBlockDataHandler() {
+        return blockDataHandler;
     }
 }
