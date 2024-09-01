e3674d7c93160c96c3523630ab8f320d2c0d6a1f
Wyzebb <BenSJSmith8@gmail.com>
2024-09-01 11:29:43 +0100

    Add logic for breaking blocks

--- src/main/java/me/wyzebb/maprepair/events/BreakBlockEvent.java
+++ src/main/java/me/wyzebb/maprepair/events/BreakBlockEvent.java
@@ -1,12 +1,32 @@
 package me.wyzebb.maprepair.events;
 
+import me.wyzebb.maprepair.MapRepair;
+import me.wyzebb.maprepair.utility.BlockDataHandler;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class BreakBlockEvent implements Listener {
+
+    private final BlockDataHandler blockDataHandler;
+    private final MapRepair plugin;
+
+    public BreakBlockEvent(MapRepair plugin) {
+        this.blockDataHandler = plugin.getBlockDataHandler();
+        this.plugin = plugin;
+    }
+
     @EventHandler
     private void onBlockBreak(BlockBreakEvent e) {
-        e.getPlayer().sendMessage("You broke " + e.getBlock());
+        final Location loc = e.getBlock().getLocation();
+        final String blockMaterial = e.getBlock().getType().toString();
+
+        if (blockDataHandler != null) {
+            blockDataHandler.saveBlockData(loc, blockMaterial);
+        } else {
+            plugin.getLogger().warning("Block data handler has not been initialised!");
+        }
     }
+
 }
