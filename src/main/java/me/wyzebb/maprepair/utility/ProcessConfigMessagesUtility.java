8c7a72cd1f2d6f0e4faac7d76c85f37d4c702ef3
Wyzebb <BenSJSmith8@gmail.com>
2024-09-02 14:41:08 +0100

    Add error color function

--- src/main/java/me/wyzebb/maprepair/utility/ProcessConfigMessagesUtility.java
+++ src/main/java/me/wyzebb/maprepair/utility/ProcessConfigMessagesUtility.java
@@ -7,19 +7,28 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 public class ProcessConfigMessagesUtility {
-    public static void processMessage(String langPath, CommandSender commandSender) {
+    public static void processMessage(String langPath, CommandSender commandSender, boolean isError) {
         MapRepair plugin = MapRepair.getPlugin(MapRepair.class);
         String color = plugin.getConfig().getString("color");
+        String errorColor = plugin.getConfig().getString("error-color");
 
         LanguageManager languageManager = plugin.getLanguageManager();
         FileConfiguration langConfig = languageManager.getLanguageFile();
 
         if (commandSender instanceof Player) {
-            commandSender.sendMessage(color + langConfig.getString(langPath, "Message key not found."));
-        }
-
-        if (commandSender instanceof ConsoleCommandSender) {
-            plugin.getLogger().info(langConfig.getString(langPath, "Message key not found."));
+            String msg;
+            if (isError) {
+                msg = errorColor;
+            } else {
+                msg = color;
+            }
+            commandSender.sendMessage(msg + langConfig.getString(langPath, "Message key not found."));
+        } else if (commandSender instanceof ConsoleCommandSender) {
+            if (isError) {
+                plugin.getLogger().warning(langConfig.getString(langPath, "Message key not found."));
+            } else {
+                plugin.getLogger().info(langConfig.getString(langPath, "Message key not found."));
+            }
         }
     }
 }
