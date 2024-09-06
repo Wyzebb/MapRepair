5a073055bd7d8c568227744ce16f1be6a785a72f
Wyzebb <BenSJSmith8@gmail.com>
2024-09-06 22:40:01 +0100

    Add event listener for blocks burning

---
+++ src/main/java/me/wyzebb/maprepair/events/BlockBurnListener.java
@@ -0,0 +1,33 @@
+package me.wyzebb.maprepair.events;
+
+import me.wyzebb.maprepair.MapRepair;
+import me.wyzebb.maprepair.utility.BlockDataHandler;
+import me.wyzebb.maprepair.utility.LanguageManager;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBurnEvent;
+
+public class BlockBurnListener implements Listener {
+
+    private final BlockDataHandler blockDataHandler;
+    private final MapRepair plugin;
+    private final LanguageManager languageManager;
+
+    public BlockBurnListener(MapRepair plugin) {
+        this.blockDataHandler = plugin.getBlockDataHandler();
+        this.plugin = plugin;
+        this.languageManager = plugin.getLanguageManager();
+    }
+
+    @EventHandler
+    private void onBlockBurn(BlockBurnEvent e) {
+        final Location loc = e.getBlock().getLocation();
+
+        if (blockDataHandler != null) {
+            blockDataHandler.saveBlockData(loc, e.getBlock().getBlockData());
+        } else {
+            plugin.getLogger().warning(languageManager.getLanguageFile().getString("error"));
+        }
+    }
+}
