1be44eeff8a5e00344c46604a4198fe5fcb8b187
Wyzebb <BenSJSmith8@gmail.com>
2024-09-29 18:17:43 +0100

    Add bed repair support

--- src/main/java/me/wyzebb/maprepair/events/BlockBreakListener.java
+++ src/main/java/me/wyzebb/maprepair/events/BlockBreakListener.java
@@ -4,6 +4,9 @@ import me.wyzebb.maprepair.MapRepair;
 import me.wyzebb.maprepair.utility.BlockDataHandler;
 import me.wyzebb.maprepair.utility.LanguageManager;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.Bed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -25,6 +28,19 @@ public class BlockBreakListener implements Listener {
         final Location loc = e.getBlock().getLocation();
 
         if (blockDataHandler != null) {
+                // Get the block's data
+                BlockData blockData = e.getBlock().getBlockData();
+
+                if (blockData instanceof Bed bedData) {
+                    // Check if this is the head or the foot of the bed
+                    if (bedData.getPart() == Bed.Part.HEAD) {
+                        Block otherHalf = e.getBlock().getRelative(bedData.getFacing().getOppositeFace());
+                        blockDataHandler.saveBlockData(otherHalf.getLocation(), otherHalf.getBlockData());
+                    } else {
+                        Block otherHalf = e.getBlock().getRelative(bedData.getFacing());
+                        blockDataHandler.saveBlockData(otherHalf.getLocation(), otherHalf.getBlockData());
+                    }
+                }
             blockDataHandler.saveBlockData(loc, e.getBlock().getBlockData());
         } else {
             plugin.getLogger().warning(languageManager.getLanguageFile().getString("error"));
--- src/main/java/me/wyzebb/maprepair/events/BlockPlaceListener.java
+++ src/main/java/me/wyzebb/maprepair/events/BlockPlaceListener.java
@@ -4,6 +4,10 @@ import me.wyzebb.maprepair.MapRepair;
 import me.wyzebb.maprepair.utility.BlockDataHandler;
 import me.wyzebb.maprepair.utility.LanguageManager;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.Bed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -25,6 +29,26 @@ public class BlockPlaceListener implements Listener {
         final Location loc = e.getBlock().getLocation();
 
         if (blockDataHandler != null) {
+            // Get the placed block's data
+            BlockData blockData = e.getBlockPlaced().getBlockData();
+
+            if (blockData instanceof Bed bedData) {
+                // Determine the other half of the bed
+                Block otherHalf;
+                if (bedData.getPart() == Bed.Part.HEAD) {
+                    // It's the head; get the foot half
+                    otherHalf = e.getBlockPlaced().getRelative(bedData.getFacing().getOppositeFace());
+                } else {
+                    // It's the foot; get the head half
+                    otherHalf = e.getBlockPlaced().getRelative(bedData.getFacing());
+                }
+
+                // Put air where the head of the bed was
+                BlockData airBlockData = Material.AIR.createBlockData();
+                blockDataHandler.saveBlockData(otherHalf.getLocation(), airBlockData);
+            }
+
+            // Save the data of the block that was there before
             blockDataHandler.saveBlockData(loc, e.getBlockReplacedState().getBlockData());
         } else {
             plugin.getLogger().warning(languageManager.getLanguageFile().getString("error"));
